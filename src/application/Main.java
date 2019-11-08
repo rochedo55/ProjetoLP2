b4bbc2d82bf2f753987c7d8b45fdaa18efbfb9c0
Anderson Pereira Torres de Sá Neto <andersonptsn@yahoo.com.br>
2019-11-08 10:14:35 -0300

    Continuando a implementação da tela principal
    ainda com dificuldades de ligar o controler com o fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,28 +1,71 @@
 package application;
 	
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import br.ufrn.imd.modelo.ObjetoDataSet;
 import br.ufrn.imd.modelo.ObjetoEuclidiano;
 import br.ufrn.imd.modelo.Tratamento;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBoxBase;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
-public class Main extends Application {
+public class Main extends Application  {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Pane root = FXMLLoader.load(getClass().getResource("..\\br\\ufrn\\imd\\visao\\TelaPrincipal.fxml"));
+			Pane root = FXMLLoader.load(getClass().getResource("../br/ufrn/imd/visao/TelaPrincipal.fxml"));
+			//"..\\br\\ufrn\\imd\\visao\\TelaPrincipal.fxml"
+			// "../br/ufrn/imd/visao/TelaPrincipal.fxml"
 			Scene scene = new Scene(root,300,150);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Titulo");
 		} catch(Exception e) {
 			e.printStackTrace();
 			
 		}
 	}
+	
+	//@FXML
+	//private Stage primaryStage;
+	
+    @FXML
+    private AnchorPane tela;
+
+    @FXML
+    private Button abrirImg;
+
+    @FXML
+    private Button verificar;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private ComboBoxBase<?> comboBox;
+	@FXML
+	public void ComboBox() {
+	    //comboBox.getId().removeAll(comboBox.getId());
+	    //comboBox.getId().addAll("Option A", "Option B", "Option C");
+	    //comboBox.getSelectionModel().select("Option B");
+	}
+	
+	@FXML
+	public void ButtonAction(ActionEvent event) {
+		System.out.print(label.getText() +"\n");
+		System.out.print("Sua mãe \n");
+		label.setText("Ola mundo");
+	}
+	
 	
 	public static void main(String[] args) {
 		// --------------------------------------> isso fica no controler ou em outra classe (provavelmente no controer msm)
@@ -31,6 +74,7 @@ public class Main extends Application {
 		tratamento.imagem("C:\\Users\\ander\\Downloads\\0.png");
 		tratamento.dataset();
 		obj.CalcularDistancia(tratamento);
+		
 		System.out.print(tratamento.getDataset().get(0).getDistancia()+"\n");
 		//System.out.print(tratamento.getDataset().get(80).getDistancia());
 		// TESTES
--- src/br/ufrn/imd/controle/ControleTela.java
+++ src/br/ufrn/imd/controle/ControleTela.java
@@ -3,35 +3,78 @@ package br.ufrn.imd.controle;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.Label;
-import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-//import br.ufrn.imd.visao.TelaPrincipal.fxml;
-import application.Main;
-
-public class ControleTela implements Initializable{
+public class ControleTela extends Application implements Initializable{
 
 	@FXML
-	private Label label;
-	@FXML
-	private ComboBoxListCell comboBox;
-	@SuppressWarnings("unchecked")
+	private Stage primeiro;
+	
+    @FXML
+    private AnchorPane tela;
+
+    @FXML
+    private Button abrirImg;
+
+    @FXML
+    private Button verificar;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private ComboBoxBase<?> comboBox;
 	@FXML
 	public void ComboBox() {
-	    comboBox.getItems().removeAll(comboBox.getItems());
-	    comboBox.getItems().addAll("Option A", "Option B", "Option C");
+	    //comboBox.getId().removeAll(comboBox.getId());
+	    //comboBox.getId().addAll("Option A", "Option B", "Option C");
 	    //comboBox.getSelectionModel().select("Option B");
 	}
-	private void ButtonAction(ActionEvent event) {
+	
+	@FXML
+	public void ButtonAction(ActionEvent event) {
+		System.out.print(label.getText() +"\n");
+		System.out.print("Sua mãe \n");
 		label.setText("Ola mundo");
 	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void initPrincipal() {
+        //essa funcao faz com que as coisas setadas no scene
+        //aparecao quando a janela for aberta,logo ela precisa ser chamada ao iniciar
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../br/ufrn/imd/visao/TelaPrincipal.fxml"));
+            loader.setController(this);
+            tela = (AnchorPane) loader.load();
 
+            Scene scene = new Scene(tela);
+            primeiro.setScene(scene);
+            primeiro.show();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
